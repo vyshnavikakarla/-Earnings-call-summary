85930c902a264b1e0607d9c034576045f3f6b5ec
Kakarla Vyshnavi <149255433+vyshnavikakarla@users.noreply.github.com>
2026-02-17 15:12:48 +0530

    Remove return message from index route
    Remove the default return message from the index route.

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 @app.route("/", methods=["GET", "POST"])
 def index():
     print("Route accessed")
-    return "Hello working"
     
     result = None
 
